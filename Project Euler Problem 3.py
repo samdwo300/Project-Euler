351bf58931d57d908500b3ef1c1b95d802d75101
samdwo300 <samdwo300@gmail.com>
2019-09-08 16:27:05 -0400

    first commit Project Euler

---
+++ Project Euler Problem 3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 12:17:04 2019
+
+@author: Samuel
+"""
+
+
+##### Problem 3 #####
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+
